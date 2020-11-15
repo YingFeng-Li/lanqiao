d1ab57ba8d091176e437442c1a5a33039e203653
Your Name <you@example.com>
2020-11-15 10:51:16 +0800

    显示hero、实现转向

---
+++ 11-14/AI/src/AI.java
@@ -0,0 +1,74 @@
+import javax.microedition.lcdui.*;
+import javax.microedition.midlet.*;
+import java.io.*;
+
+public class AI extends MIDlet
+{
+	Display display;
+	MainCanvas mc;
+	public AI(){
+		display=Display.getDisplay(this);
+		mc=new MainCanvas();
+		display.setCurrent(mc);
+	}
+	public void startApp(){
+	}
+	public void destroyApp(boolean unc){
+	}
+	public void pauseApp(){
+	}
+}
+class MainCanvas extends Canvas
+{	
+	Image currentImg,img1,img2;
+	Image img3,img4;
+	public MainCanvas(){
+		try
+		{
+			img1= Image.createImage("/sayo10.png");
+			img2= Image.createImage("/sayo12.png");
+			img3= Image.createImage("/sayo04.png");//up
+			img4= Image.createImage("/sayo16.png");//right
+			currentImg=img1;
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	
+	
+	
+	}
+	public void paint(Graphics g){
+		g.setColor(0,0,0);
+		g.fillRect(0,0,getWidth(),getHeight());
+		g.drawImage(currentImg,120,120,0);
+	}
+
+public void keyPressed(int keyCode){
+
+	int action=getGameAction(keyCode);
+	
+	if(action==LEFT){
+		System.out.println("left");
+		currentImg=img2;
+		repaint();
+		}
+	else if(action==UP){
+		System.out.println("up");
+		currentImg=img3;
+		repaint();
+		}
+	else if(action==DOWN){
+		System.out.println("down");
+		currentImg=img1;
+		repaint();
+		}
+	else if(action==RIGHT){
+		System.out.println("right");
+		currentImg=img4;
+		repaint();
+		}
+	}
+}
+
