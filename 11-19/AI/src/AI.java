22666d3557daf012898703e85651791c7ad4d81b
Your Name <you@example.com>
2020-11-19 23:02:48 +0800

    使用for循环以及数组实现人物四个方向的移动

---
+++ 11-19/AI/src/AI.java
@@ -0,0 +1,145 @@
+import javax.microedition.lcdui.*;
+import javax.microedition.midlet.*;
+import java.io.*;
+
+public class AI extends MIDlet
+{
+	Display display;
+	MainCanvas mc;
+	public AI(){
+		display=Display.getDisplay(this);
+		mc=new MainCanvas();
+		display.setCurrent(mc);
+	}
+	public void startApp(){
+	}
+	public void destroyApp(boolean unc){
+	}
+	public void pauseApp(){
+	}
+}
+class MainCanvas extends Canvas
+{	
+	//坐标与移动判断的定义
+	int x,y;
+	int leftFlag,rightFlag,upFlag,downFlag;
+
+	Image currentImg;
+	Image heroLeftImage[]=new Image[3];
+	Image heroRightImage[]=new Image[3];
+	Image heroUpImage[]=new Image[3];
+	Image heroDownImage[]=new Image[3];
+
+	public MainCanvas(){
+		try
+		{
+			
+			//左
+			for(int i=0;i<heroLeftImage.length;i++){
+				heroLeftImage[i]=Image.createImage("/sayo"+i+"2.png");
+			}
+
+			//右
+			for(int i=0;i<heroRightImage.length;i++){
+				heroRightImage[i]=Image.createImage("/sayo"+i+"6.png");
+			}
+
+			//上
+			for(int i=0;i<heroUpImage.length;i++){
+				heroUpImage[i]=Image.createImage("/sayo"+i+"4.png");
+			}
+			
+			//下
+			for(int i=0;i<heroDownImage.length;i++){
+				heroDownImage[i]=Image.createImage("/sayo"+i+"0.png");
+			}
+
+
+			//给变量赋初值
+			leftFlag=1;
+			rightFlag=1;
+			upFlag=1;
+			downFlag=1;
+			currentImg=heroLeftImage[1];
+
+			x=120;
+			y=120;
+
+
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	
+	
+	
+	}
+	public void paint(Graphics g){
+		g.setColor(0,0,0);
+		g.fillRect(0,0,getWidth(),getHeight());
+		g.drawImage(currentImg,x,y,0);
+	}
+
+public void keyPressed(int keyCode){
+
+	int action=getGameAction(keyCode);
+	
+	if(action==LEFT){
+		if(leftFlag==1){
+			currentImg=heroLeftImage[0];
+			leftFlag++;
+		}
+		else if(leftFlag==2){
+			currentImg=heroLeftImage[2];
+			leftFlag=1;
+		}
+		System.out.println("left");
+		x--;
+		repaint();
+		}
+
+	else if(action==UP){
+		if(upFlag==1){
+			currentImg=heroUpImage[0];
+			upFlag++;
+		}
+		else if(upFlag==2){
+			currentImg=heroUpImage[2];
+			upFlag=1;
+		}
+		System.out.println("up");
+		y--;
+		repaint();
+		}
+
+	else if(action==DOWN){
+		if(downFlag==1){
+			currentImg=heroDownImage[0];
+			downFlag++;
+		}
+		else if(downFlag==2){
+			currentImg=heroDownImage[2];
+			downFlag=1;
+		}
+		System.out.println("down");
+		y++;
+		repaint();
+		}
+
+	else if(action==RIGHT){
+		if(rightFlag==1){
+			currentImg=heroRightImage[0];
+			rightFlag++;
+		}
+		else if(rightFlag==2){
+			currentImg=heroRightImage[2];
+			rightFlag=1;
+		}
+		x++;
+		System.out.println("right");
+		repaint();
+		}
+	}
+}
+
